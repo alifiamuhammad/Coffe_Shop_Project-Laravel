b2d4de2231d54a2ab4ce2f544df994e22fb5df41
alifiamuhammad <72847257+alifiamuhammad@users.noreply.github.com>
2023-11-22 03:19:19 +0700

    front end 2

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ServiceController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,3 +22,4 @@ Route::get('/', function () {
 Route::get('/login', [LoginController::class, 'index']);
 Route::get('/menu', [MenuController::class, 'index']);
 Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/service', [serviceController::class, 'index']);
